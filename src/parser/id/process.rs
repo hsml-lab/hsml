6f6c2afd2ddb12460a1f3b839f26390a1905d543
Shinigami92 <chrissi92@hotmail.de>
2026-03-19 19:50:35 +0100

    feat(parser): enhance ID processing to support hyphens and underscores

--- src/parser/id/process.rs
+++ src/parser/id/process.rs
@@ -1,9 +1,9 @@
-use nom::{IResult, bytes::complete::tag};
+use nom::{IResult, bytes::complete::tag, bytes::complete::take_while1};
 
 pub(super) fn process_id(input: &str) -> IResult<&str, &str> {
     let (input, _) = tag("#")(input)?;
 
-    let (input, id) = nom::character::complete::alphanumeric1(input)?;
+    let (input, id) = take_while1(|c: char| c.is_alphanumeric() || c == '-' || c == '_')(input)?;
 
     Ok((input, id))
 }
