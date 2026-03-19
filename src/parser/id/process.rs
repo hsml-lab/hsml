86f11215eb77ccdc32763d7e04498195744af915
Shinigami92 <chrissi92@hotmail.de>
2026-03-19 19:59:15 +0100

    feat(parser): update ID processing to ensure ASCII alphanumeric support

--- src/parser/id/process.rs
+++ src/parser/id/process.rs
@@ -3,7 +3,8 @@ use nom::{IResult, bytes::complete::tag, bytes::complete::take_while1};
 pub(super) fn process_id(input: &str) -> IResult<&str, &str> {
     let (input, _) = tag("#")(input)?;
 
-    let (input, id) = take_while1(|c: char| c.is_alphanumeric() || c == '-' || c == '_')(input)?;
+    let (input, id) =
+        take_while1(|c: char| c.is_ascii_alphanumeric() || c == '-' || c == '_')(input)?;
 
     Ok((input, id))
 }
