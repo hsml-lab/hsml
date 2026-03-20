73f993f554e63988b94662262d42df73bbe37eda
Shinigami <chrissi92@hotmail.de>
2026-03-20 22:05:34 +0100

    fix: toggle escape state instead of always setting true (#47)

--- src/parser/class/tests/process.rs
+++ src/parser/class/tests/process.rs
@@ -134,6 +134,46 @@ fn it_should_process_class_with_multibyte_chars() {
 }
 
 #[test]
+fn it_should_process_class_with_escaped_bracket() {
+    let input = Span::new(r".[a\]b].next");
+
+    let (rest, class) = process_class(input).unwrap();
+
+    assert_eq!(*class.fragment(), r"[a\]b]");
+    assert_eq!(*rest.fragment(), ".next");
+}
+
+#[test]
+fn it_should_process_class_with_double_backslash_before_bracket() {
+    let input = Span::new(r".[a\\].next");
+
+    let (rest, class) = process_class(input).unwrap();
+
+    assert_eq!(*class.fragment(), r"[a\\]");
+    assert_eq!(*rest.fragment(), ".next");
+}
+
+#[test]
+fn it_should_process_class_with_only_escaped_backslash_in_brackets() {
+    let input = Span::new(r".bg-[\\].text-white Hello");
+
+    let (rest, class) = process_class(input).unwrap();
+
+    assert_eq!(*class.fragment(), r"bg-[\\]");
+    assert_eq!(*rest.fragment(), ".text-white Hello");
+}
+
+#[test]
+fn it_should_process_class_with_windows_path_in_brackets() {
+    let input = Span::new(r".bg-[url('C:\\images\\bg.png')].text-white Hello");
+
+    let (rest, class) = process_class(input).unwrap();
+
+    assert_eq!(*class.fragment(), r"bg-[url('C:\\images\\bg.png')]");
+    assert_eq!(*rest.fragment(), ".text-white Hello");
+}
+
+#[test]
 fn it_should_process_class_with_multibyte_arbitrary_value() {
     let input = Span::new(".bg-[ä]#id");
 
